7e06e21478dc60d3d51aab9836ffadf956302964
OleHolmNielsen <Ole.H.Nielsen@fysik.dtu.dk>
2024-09-30 10:23:04 +0200

    usleep command is deprecated

--- nodes/update.sh
+++ nodes/update.sh
@@ -101,7 +101,10 @@ function crontab_cleanup ()
 # Check for a lock file and exit if it exists.
 # Duplicates may happen if multiple lines have been added to /etc/crontab by mistake.
 # Sleep a random number of microseconds in order to avoid a race condition:
-usleep $RANDOM
+sleep `awk -v ran=$RANDOM 'BEGIN{printf("%10.6f\n"), ran / 1000000}'`
+# The usleep command is deprecated:
+# usleep $RANDOM
+
 if [ -e $LOCK_FILE ]
 then
 	echo "ERROR: Stop file $LOCK_FILE already exists, exiting..."
